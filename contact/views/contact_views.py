5d7a0d5a740708eb087decbcbafea3047e24c73a
Marcelo Henrique <marcelohenriquex77@gmail.com>
2023-09-20 21:35:28 -0300

    commi

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from django.db.models import Q
 from contact.models import Contact
-from django.http import Http404
 
 
 def index(request):
     contacts = Contact.objects \
             .filter(show=True)\
-            .order_by('-id')[0:10]
+            .order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     
     print(contacts.query)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -59,10 +63,12 @@ def search(request):
         )\
         .order_by('-id')
 
-    print(contacts.query)
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - ',
         'search_value': search_value,
     }
